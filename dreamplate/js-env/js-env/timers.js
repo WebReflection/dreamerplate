457063150ac700e8d28f2d3e4de8adbd4ebb0884
Andrea Giammarchi <agiammarchi@twitter.com>
2013-10-03 21:07:23 -0700

    removed wrong directory

--- dreamplate/js-env/js-env/timers.js
+++
@@ -1,6 +0,0 @@
-/*@cc_on
-(function(f){
-window.setTimeout=f(window.setTimeout);
-window.setInterval=f(window.setInterval);
-})(function(f){return function(c,t,a){a=[].slice.call(arguments,2);return f(function(){c.apply(this,a)},t)}});
-@*/
